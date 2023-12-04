126c06ca835aa2b368cecaf4a7a640cf5d733030
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-12-04 18:11:03 +0100

    do not attempt to get diff of deleted files

--- lua/git_diff/init.lua
+++ lua/git_diff/init.lua
@@ -21,6 +21,10 @@ m.modified_on_current_branch = function()
             sorter = sorters.get_fuzzy_file(),
             previewer = previewers.new_termopen_previewer({
                 get_command = function(entry)
+                    if vim.fn.filereadable(entry.value) == 0 then
+                        return { "echo", "File was deleted." }
+                    end
+
                     return {
                         "git",
                         "diff",
